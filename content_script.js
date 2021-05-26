e6637ff4a9db630cad2b4da9b178a8e440bd248c
Ganesh Katrapati <ganesh@swecha.net>
2021-05-26 15:29:47 +0530

    Language change when pressed Go

--- content_script.js
+++ content_script.js
@@ -1,63 +1,59 @@
-/*window.onload = (event) => {
-  loadLocalization("te");
-};*/
-document.addEventListener('DOMContentLoaded', function() {
-
-  document.getElementById("Go").onclick = function(){
-    var lang_code = document.getElementById("language");
-    console.log(lang_code.value);
-    loadLocalization(lang_code.value);
-  }
-  console.log("hello");
-});
-
+var browser = browser || chrome;
+var originals = [];
+var existingLocale = "en";
 
 function loadLocalization(lang){
-console.log("Localization Loading . . . ");
-let URL = browser.extension.getURL("data/json/"+lang+".json");
-window.fetch(URL)
-.then((res) => res.json())
-.then((strings) => {
-  translations = JSON.parse(JSON.stringify(strings));
-  console.log("Fetch Successful:: " + JSON.stringify(translations));
-}).then(() => {
-  const elementsInsideBody = [...document.body.getElementsByTagName("*")]
-  findAndReplace(elementsInsideBody);
-});
-}
+  console.log("Localization Loading . . . ");
+  let URL = browser.runtime.getURL("data/json/"+lang+".json");
+  window.fetch(URL)
+  .then((res) => res.json())
+  .then((strings) => {
+    var translations = JSON.parse(JSON.stringify(strings));
+    const elementsInsideBody = [...document.body.getElementsByTagName("*")]
 
-/**
-* This makes an array of everything elements inside the body tag
-*/
-// document.body.onload = function () {
-//   const elementsInsideBody = [...document.body.getElementsByTagName("*")]
-//   window.onload = findAndReplace(elementsInsideBody)
-// }
-
-/**
-* A function that loops through every single item
-* @param {*} elementsInsideBody
-*/
-//
-function findAndReplace(elementsInsideBody) {
-elementsInsideBody.forEach((element) => {
-  element.childNodes.forEach((child) => {
-    if (child.nodeType === 3) {
-      replaceText(child)
-    }
+    findAndReplace(elementsInsideBody, translations);
+    existingLocale = lang;
+    // console.log("Fetch Successful:: " + JSON.stringify(translations2));
   })
-})
 }
 
 /**
-* Regex Function to replace the node
-* @param {*} node
-*/
-function replaceText(node) {
-let value = node.nodeValue
-translations.map((obj) => {
-  const regexVariable = new RegExp(obj.word, "g")
-  value = value.replace(regexVariable, obj.replacedWord)
-  node.nodeValue = value
-})
+ * A function that loops through every single item
+ * @param {*} elementsInsideBody
+ */
+//
+function findAndReplace(elementsInsideBody, translations) {
+  elementsInsideBody.forEach((element) => {
+    element.childNodes.forEach((child) => {
+      if (child.nodeType === 3) {
+        replaceText(child, translations)
+      }
+    })
+  })
 }
+
+/**
+ * Regex Function to replace the node
+ * @param {*} node
+ */
+function replaceText(node, translations) {
+  let value = node.nodeValue
+  translations.map((obj) => {
+    const regexVariable = new RegExp(obj.word, "g")
+    value = value.replace(regexVariable, obj.replacedWord)
+   
+    node.nodeValue = value
+  })
+}
+
+
+browser.runtime.onMessage.addListener( function(request, sender, sendResponse) {
+    console.log("something happening from the extension");
+    var data = request.data || {};
+    console.log(data);
+    if (data.lang == 'en') {
+    	location.reload();
+    }
+    loadLocalization(data.lang);
+    sendResponse({data: {message : "success"}, success: true});
+});
